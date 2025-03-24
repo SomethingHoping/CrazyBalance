c4e0781778cf79417a67a176b01c3e4f98ca8bdc
NathAchil <nathaniel16achilles@gmail.com>
2025-03-24 19:30:49 -0400

    Delete old folder

--- gTI/core/src/main/java/com/badlogic/crazybalance/BattlingUI.java
+++
@@ -1,7 +0,0 @@
-package com.badlogic.crazybalance;
-
-import com.badlogic.gdx.scenes.scene2d.Stage;
-
-public class BattlingUI extends Stage{
-
-}
--- gTI/core/src/main/java/com/badlogic/crazybalance/MovementSystem.java
+++
@@ -1,7 +0,0 @@
-package com.badlogic.crazybalance;
-
-import com.badlogic.ashley.core.EntitySystem;
-
-public class MovementSystem extends EntitySystem{
-
-}
